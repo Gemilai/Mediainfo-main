9de3ce03e7e3888b43f78816765b164cb1223572
Gemilai <youtakefreshfig@gmail.com>
2025-12-16 19:40:12 +0330

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
-import { index, type RouteConfig } from '@react-router/dev/routes';
+import { index, route, type RouteConfig } from '@react-router/dev/routes';
 
 export default [
   index('routes/_index.tsx'),
+  route('login', 'routes/login.tsx'), // Added this line
   { file: 'routes/home.tsx', path: 'home' },
 ] satisfies RouteConfig;
